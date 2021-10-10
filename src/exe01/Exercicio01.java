ec75f9d8895cf3310af28fb697b84a289dd5761a
Airk-Souza <airkdesouza@gmail.com>
2021-10-10 12:27:56 -0300

    Iniciando a lista

---
+++ src/exe01/Exercicio01.java
@@ -0,0 +1,9 @@
+package exe01;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++){
+            System.out.println("Eu amo algoritmos!");
+        }
+    }
+}
