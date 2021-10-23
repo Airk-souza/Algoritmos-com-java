98f4188d19a68125a331856e01f09f60f5a7225f
Airk-souza <airkdesouza@gmail.com>
2021-10-23 17:20:43 -0300

    Organizando

--- src/exe01/Exercicio01.java
+++
@@ -1,9 +0,0 @@
-package exe01;
-//1)   Escreva um algoritmo que exiba 20 vezes a mensagem “Eu gosto de estudar Algoritmos!”.
-public class Exercicio01 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 20; i++){
-            System.out.println("Eu amo algoritmos!");
-        }
-    }
-}
