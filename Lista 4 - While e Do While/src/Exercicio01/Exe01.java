991b19aea95f8eb88913b118d6deb411e587e647
Airk-souza <airkdesouza@gmail.com>
2021-10-23 22:19:36 -0300

    Exercício 1

--- Lista 4 - While e Do While/src/Exercicio01/Exe01.java
+++ Lista 4 - While e Do While/src/Exercicio01/Exe01.java
@@ -1,7 +1,24 @@
 package Exercicio01;
+// Construir um algoritmo que calcule a média aritmética de vários valores inteiros positivos.
+// O final da leitura acontecerá quando for lido um valor negativo.
+
+import java.util.Scanner;
 
 public class Exe01 {
     public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
+        int numero, contador = 0, soma=0;
+        Scanner read = new Scanner(System.in);
+        System.out.println("Digite os numeros (Um número negativo para sair)");
+        numero = read.nextInt();
+
+        while (numero >= 0){
+             soma = numero + soma;
+             contador++;
+             numero = read.nextInt();
+        }
+
+        float media = (float)soma/contador;
+
+        System.out.println("Media: "+ media + "\nSoma dos números: " +soma +"\nQuantidade de números: "+ contador);
     }
 }
